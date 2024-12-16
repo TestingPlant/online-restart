1d63a0a3b61ca4a6aa8e5cdb21ea239472f7d793
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2024-12-16 14:28:59 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ static RESTART_LOCK: Mutex<()> = Mutex::const_new(());
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(get_handler).post(post_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    restart().await;
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -27,17 +28,7 @@ async fn get_handler() -> Response {
 async fn post_handler(headers: HeaderMap) -> Response {
     if let Some(key) = headers.get("KEY") {
         if key.as_bytes() == include_str!("../secret").trim().as_bytes() {
-            eprintln!("acquiring restart lock");
-            let _lock = RESTART_LOCK.lock().await;
-            eprintln!("restarting");
-            let _ = Command::new("podman").arg("pull").arg("ghcr.io/andrewgazelka/hyperion/tag:latest").status().await;
-            let _ = Command::new("podman").arg("pull").arg("ghcr.io/andrewgazelka/hyperion/hyperion-proxy:latest").status().await;
-            let _ = Command::new("podman").arg("stop").arg("tag").status().await;
-            let _ = Command::new("podman").arg("stop").arg("hyperion-proxy").status().await;
-            let _ = Command::new("podman").arg("rm").arg("hyperion-proxy").status().await;
-            let _ = Command::new("podman").arg("run").arg("-d").arg("--name").arg("tag").arg("-p").arg("127.0.0.1:35565:35565").arg("tag").status().await;
-            let _ = Command::new("podman").arg("run").arg("-d").arg("--name").arg("tag").arg("-p").arg("10.0.0.41:25565:25565").arg("hyperion-proxy").arg("0.0.0.0:25565").status().await;
-            eprintln!("finished restart");
+            restart().await;
             return Response::builder()
                 .status(200)
                 .body(Body::empty())
@@ -49,4 +40,19 @@ async fn post_handler(headers: HeaderMap) -> Response {
         .status(401)
         .body(Body::empty())
         .unwrap()
+}
+
+async fn restart() {
+    eprintln!("acquiring restart lock");
+    let _lock = RESTART_LOCK.lock().await;
+    eprintln!("restarting");
+    let _ = Command::new("podman").arg("pull").arg("ghcr.io/hyperion-mc/hyperion/tag:latest").status().await;
+    let _ = Command::new("podman").arg("pull").arg("ghcr.io/hyperion-mc/hyperion/hyperion-proxy:latest").status().await;
+    let _ = Command::new("podman").arg("stop").arg("tag").status().await;
+    let _ = Command::new("podman").arg("stop").arg("proxy").status().await;
+    let _ = Command::new("podman").arg("rm").arg("tag").status().await;
+    let _ = Command::new("podman").arg("rm").arg("proxy").status().await;
+    let _ = Command::new("podman").arg("run").arg("-d").arg("--name").arg("tag").arg("--pod").arg("hyperion").arg("tag").status().await;
+    let _ = Command::new("podman").arg("run").arg("-d").arg("--name").arg("proxy").arg("--pod").arg("hyperion").arg("hyperion-proxy").arg("0.0.0.0:25565").arg("--server").arg("127.0.0.1:35565").status().await;
+    eprintln!("finished restart");
 }
